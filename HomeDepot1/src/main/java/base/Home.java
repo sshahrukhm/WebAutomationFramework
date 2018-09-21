3290259c9d5900f50a9b64f1f23c4221c0e70936
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-21 13:53:10 -0400

    baseUpdated

--- HomeDepot1/src/main/java/base/Home.java
+++ HomeDepot1/src/main/java/base/Home.java
@@ -1,5 +1,17 @@
 package base;
 
-public class Home {
-    
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
+
+public class Home extends CommonAPI {
+    ////input[@id='headerSearch']
+    @FindBy(how = How.ID, using = "//input[@id='headerSearch']")
+    public static WebElement searchbox;
+
+    @FindBy(how = How.ID, using = "//div[@id='container']//div[@class='Header3']//ul[@class='ShoppingLinks']//a[@href='//www.homedepot.com/c/site_map']")
+    public static WebElement allDepartments;
+
 }
---
+++ HomeDepot1/src/test/java/get/HomePage.java
@@ -0,0 +1,4 @@
+package get;
+
+public class HomePage {
+}
